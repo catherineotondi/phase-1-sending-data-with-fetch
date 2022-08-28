a03d1173bbde1c17a1a17be90af4bd6e0c4aae2c
catherineotondi <catherineotondi@gmail.com>
2022-08-28 18:24:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Add your code here
+// // const configurationObject = {
+// //     method: "POST",
+// //     headers: {
+// //       "Content-Type": "application/json",
+// //       Accept: "application/json",
+// //     },
+// //     body: JSON.stringify({
+// //       dogName: "Byron",
+// //       dogBreed: "Poodle",
+// //     }),
+// //   };
+  
+// //   fetch("http://localhost:3000/dogs", configurationObject)
+// //   .then(function (response) {
+// //     return response.json();
+// //   })
+// //   .then(function (object) {
+// //     console.log(object);
+// //   });
+
+
+
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+    },
+    body: JSON.stringify({
+              name: name,
+              email: email,
+            })
+}
+    return fetch("http://localhost:3000/users", configurationObject)
+        
+        .then(resp => resp.json())
+        .then(data => document.querySelector('body').innerHTML=data.id)
+        .catch(error =>{
+            document.querySelector('body').innerHTML=error.message
+    
+        })
+    
+ }
+
+
+
+
